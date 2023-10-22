43b5dc53c59173b198f5078e7da2589c52dcdb6c
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2023-10-22 10:35:57 +0200

    adds Parallax

---
+++ Parallax/Layer.js
@@ -0,0 +1,22 @@
+class Layer {
+  constructor(image, speedModifier) {
+    this.x = 0;
+    this.y = 0;
+    this.width = 2400;
+    this.height = 700;
+    this.x2 = this.width;
+    this.image = image;
+    this.speedModifier = speedModifier;
+    this.speed = gameSpeed * this.speedModifier;
+  }
+
+  update() {
+    this.speed = gameSpeed * this.speedModifier;
+    if (this.x <= -this.width) {
+      this.x = this.width + this.x2 - this.speed;
+    }
+    if (this.x2 <= -this.width) {
+      this.x2 = this.width + this.x - this.speed;
+    }
+  }
+}
