ceb22ff17a4edf51673263ac71d890cb1543d7bf
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2023-10-22 11:07:05 +0200

    adds slider

--- Parallax/script.js
+++ Parallax/script.js
@@ -3,7 +3,8 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = (canvas.width = 800);
 const CANVAS_HEIGHT = (canvas.height = 700);
 
-let gameSpeed = 5;
+const slider = document.getElementById('slider');
+const showGameSpeed = document.getElementById('showGameSpeed');
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = './images/layer-1.png';
@@ -22,7 +23,7 @@ class Layer {
     this.y = 0;
     this.width = 2400;
     this.height = 700;
-    this.x2 = this.width;
+
     this.image = image;
     this.speedModifier = speedModifier;
     this.speed = gameSpeed * this.speedModifier;
@@ -31,20 +32,31 @@ class Layer {
   update() {
     this.speed = gameSpeed * this.speedModifier;
     if (this.x <= -this.width) {
-      this.x = this.width + this.x2 - this.speed;
+      this.x = 0;
     }
-    if (this.x2 <= -this.width) {
-      this.x2 = this.width + this.x - this.speed;
-    }
+
     this.x = Math.floor(this.x - this.speed);
-    this.x2 = Math.floor(this.x2 - this.speed);
   }
 
   draw() {
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.x + this.width,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
+
+let gameSpeed = 5;
+slider.value = gameSpeed;
+showGameSpeed.innerHTML = gameSpeed;
+slider.addEventListener('change', function (e) {
+  gameSpeed = e.target.value;
+  showGameSpeed.innerHTML = gameSpeed;
+});
 
 const layer5 = new Layer(backgroundLayer5, 0.8);
 const layer4 = new Layer(backgroundLayer4, 0.6);
@@ -64,4 +76,4 @@ function animate() {
   requestAnimationFrame(animate);
 }
 
-// animate();
+animate();
